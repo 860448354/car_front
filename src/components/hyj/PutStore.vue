62845850c2a75d36828bb1d078a626e1d64a993d
明月 <860448354@qq.com>
2021-11-07 16:06:57 +0800

    1

---
+++ src/components/hyj/PutStore.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "PutStore",
+  data(){
+    return{
+
+    }
+  },methods:{
+
+  },created() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
