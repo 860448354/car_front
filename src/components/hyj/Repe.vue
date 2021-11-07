bab02722d591a16b8e204d56769458423834e30e
明月 <860448354@qq.com>
2021-11-08 02:32:41 +0800

    1

--- src/components/hyj/Repe.vue
+++ src/components/hyj/Repe.vue
@@ -1,30 +1,73 @@
 <template>
+  <el-select v-model="store2" style="margin-top: 20px;margin-left: 20px" @change="test" placeholder="请选择">
+    <el-option
+        v-for="item in store"
+        :key="item.storeName"
+        :label="item.storeName"
+        :value="item.storeName">
+    </el-option>
+  </el-select>
   <el-table
-      :data="putSto.purchase.purXq"
+      :data="tableData"
       :header-cell-style="{textAlign: 'center'}"
       :cell-style="{ textAlign: 'center' }"
       style="width: 100%">
-    <el-table-column prop="commName" label="配件名称"></el-table-column>
-    <el-table-column prop="commSpe" label="配件规格"></el-table-column>
-    <el-table-column prop="commCar" label="配件车型"></el-table-column>
-    <el-table-column prop="commUnit" label="单位"></el-table-column>
-    <el-table-column prop="commMoney" label="单价"></el-table-column>
-    <el-table-column prop="commNum" label="购买数量"></el-table-column>
-    <el-table-column prop="commSNum" width="200px" label="实际数量">
-      <template v-slot="y">
-        <el-input-number
-            v-model="y.row.commSNum"
-            controls-position="right"
-            size="mini"
-            :min="0"></el-input-number>
+    <el-table-column prop="storeName" label="仓库名称">
+      <template v-slot:default="r">
+        {{r.row.store[0].storeName}}
       </template>
     </el-table-column>
+    <el-table-column prop="supplierName" label="供货商"></el-table-column>
+    <el-table-column prop="repeCommName" label="配件名称"></el-table-column>
+    <el-table-column prop="repeCommSpe" label="配件规格"></el-table-column>
+    <el-table-column prop="repeCommCar" label="车型"></el-table-column>
+    <el-table-column prop="repeCommUnit" label="单位"></el-table-column>
+    <el-table-column prop="repeCommNum" label="数量"></el-table-column>
   </el-table>
 </template>
 
 <script>
 export default {
-name: "Repe"
+name: "Repe",
+  data(){
+    return{
+      tableData:[{
+        storeName:'',
+        supplierName:'',
+        commName:'',
+        commSpe:'',
+        commCar:'',
+        CommUnit:'',
+        commNum:'',
+        comms:[{
+          supplierName:''
+        }],
+        store:[{
+          storeName:''
+        }]
+      }],
+      store:[{
+        storeName:''
+      }],
+      store2:'一号仓库'
+    }
+  },
+  methods:{
+    findStore(){
+      this.axios.get("hyj/findAllStore").then(res=>{
+        this.store = res.data;
+      })
+    },
+    test(){
+      this.axios({url:"hyj/findRe",params:{store:this.store2}}).then(res=>{
+        this.tableData = res.data
+      })
+    }
+  },
+  created() {
+    this.findStore();
+    this.test()
+  }
 }
 </script>
 
