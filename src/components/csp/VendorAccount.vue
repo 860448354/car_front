ee7c0691b8697e3e413bc0144d0a1d9001dbfaa7
Administrator <11111111>
2021-11-08 10:20:54 +0800

    供货商结算校验

--- src/components/csp/VendorAccount.vue
+++ src/components/csp/VendorAccount.vue
@@ -49,18 +49,21 @@
 		</el-table>
 	</el-row>
 	<el-dialog title="付款" v-model="dialogFormVisible" width="30%" @close="clearForm()">
-		<el-form :model="vendorAccount" label-width="80px">
+		<el-form ref="vendorAccount" :model="vendorAccount" :rules="vendorAccounts" label-width="80px">
 			<el-form-item prop="purOrder" label="订单号">
-				<el-input v-model="vendorAccount.purOrder" disabled ></el-input>
+				<el-input v-model="vendorAccount.purOrder" disabled></el-input>
 			</el-form-item>
 			<el-form-item prop="vendorDue" label="应付金额">
-				<el-input v-model="vendorAccount.vendorDue" disabled ></el-input>
-			</el-form-item>
-			<el-form-item prop="vendorReceived" label="付款金额">
-				<el-input v-model="vendorAccount.vendorReceived"></el-input>
+				<el-input v-model="vendorAccount.vendorDue" disabled></el-input>
 			</el-form-item>
 			<el-form-item prop="vendorCoupon" label="优惠金额">
-				<el-input v-model="vendorAccount.vendorCoupon"></el-input>
+				<el-input v-model="vendorAccount.vendorCoupon" type="number" @keydown="handleInput2"></el-input>
+			</el-form-item>
+			<el-form-item prop="vendorMoney" label="实际金额">
+				<el-input v-model="vendorAccount.vendorMoney" type="number" @keydown="handleInput2" disabled></el-input>
+			</el-form-item>
+			<el-form-item prop="vendorReceived" label="付款金额">
+				<el-input type="number" v-model="vendorAccount.vendorReceived" @keydown="handleInput2"></el-input>
 			</el-form-item>
 			<el-form-item prop="vendorEnter" label="入账方式">
 				<el-input v-model="vendorAccount.vendorEnter"></el-input>
@@ -68,7 +71,7 @@
 		</el-form>
 		<template #footer>
 			<span class="dialog-footer">
-				<el-button @click="clearForm()">取 消</el-button>
+				<el-button @click="clearForm(),dialogFormVisible = false">取 消</el-button>
 				<el-button type="primary" @click="getPayment()">确 定</el-button>
 			</span>
 		</template>
@@ -89,14 +92,31 @@
 					vendorDue: 0,
 					vendorReceived: 0,
 					vendorCoupon: 0,
+					vendorMoney: 0,
 					vendorEnter: '',
 					vendorType: '配件结算单',
 					sfId: '',
 					sfName: '',
+				},
+				vendorAccounts: {
+					vendorReceived: [{
+						required: true,
+						message: '请输入付款金额',
+						trigger: 'blur'
+					}],
+					vendorEnter: [{
+						required: true,
+						message: '请输入入账方式',
+						trigger: 'blur'
+					}],
 				}
 			}
 		},
 		methods: {
+			handleInput2(e) {
+				// 通过正则过滤小数点后两位
+				e.target.value = (e.target.value.match(/^\d*(\.?\d{0,1})/g)[0]) || null
+			},
 			getPurchase() {
 				this.axios({
 					url: '/account/vendor-account',
@@ -104,8 +124,6 @@
 						supplierName: this.supplierName
 					}
 				}).then((res) => {
-					console.log("11111")
-					console.log(res)
 					this.purchase = res.data;
 				}).catch(() => {})
 			},
@@ -118,27 +136,41 @@
 			getPayment() {
 				this.vendorAccount.sfId = this.$store.state.message.myStaff.sfId;
 				this.vendorAccount.sfName = this.$store.state.message.myStaff.sfName;
-				this.axios.post(
-					'/account/add-vendorAccount',
-					this.vendorAccount
-				).then((res) => {
-					if (res.data == 'ok') {
-						this.$message.success("新增成功");
-						this.vendorAccount.purOrder = '';
-						this.vendorAccount.supplierName = '';
-						this.vendorAccount.vendorDue = 0;
-						this.vendorAccount.vendorReceived = 0;
-						this.vendorAccount.vendorCoupon = 0;
-						this.vendorAccount.vendorEnter = '';
-						this.vendorAccount.vendorType = '';
-						this.vendorAccount.sfId = '';
-						this.dialogFormVisible = false;
-						this.getPurchase();
-						this.purXq = [];
-					} else if (v.data == 'fail') {
-						this.$message.error("新增失败");
+				this.$refs['vendorAccount'].validate((valid) => {
+					if (valid) {
+						if (this.vendorAccount.vendorReceived == this.vendorAccount.vendorMoney) {
+							this.axios.post(
+								'/account/add-vendorAccount',
+								this.vendorAccount
+							).then((res) => {
+								if (res.data == 'ok') {
+									this.$message.success("新增成功");
+									this.vendorAccount.purOrder = '';
+									this.vendorAccount.supplierName = '';
+									this.vendorAccount.vendorDue = 0;
+									this.vendorAccount.vendorReceived = 0;
+									this.vendorAccount.vendorCoupon = 0;
+									this.vendorAccount.vendorMoney = 0;
+									this.vendorAccount.vendorEnter = '';
+									this.vendorAccount.vendorType = '';
+									this.vendorAccount.sfId = '';
+									this.dialogFormVisible = false;
+									this.getPurchase();
+									this.purXq = [];
+								} else if (v.data == 'fail') {
+									this.$message.error("新增失败");
+								}
+							}).catch(() => {})
+						} else if (this.vendorAccount.vendorReceived < this.vendorAccount.vendorMoney) {
+							this.$message.error("不可低于实际应付金额");
+						} else if (this.vendorAccount.vendorReceived > this.vendorAccount.vendorMoney) {
+							this.$message.error("不可高于实际应付金额");
+						}
+					} else {
+						this.$message.error("请输入必填项");
+						return false;
 					}
-				}).catch(() => {})
+				})
 			},
 			clearForm() {
 				this.vendorAccount.purOrder = '';
@@ -146,10 +178,28 @@
 				this.vendorAccount.vendorDue = 0;
 				this.vendorAccount.vendorReceived = 0;
 				this.vendorAccount.vendorCoupon = 0;
+				this.vendorAccount.vendorMoney = 0;
 				this.vendorAccount.vendorEnter = '';
 				this.vendorAccount.vendorType = '';
 				this.vendorAccount.sfId = '';
 			}
+		},
+		//计算属性
+		computed: {
+			differ: function() {
+				let sum = 0;
+				sum = this.vendorAccount.vendorDue - this.vendorAccount.vendorCoupon;
+				return sum;
+			},
+		},
+		//计算找零
+		watch: {
+			differ: {
+				deep: true,
+				handler: function(newVal) {
+					this.vendorAccount.vendorMoney = newVal
+				},
+			},
 		},
 		created() {
 			this.getPurchase();
